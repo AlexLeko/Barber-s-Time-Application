1a05c96bc4c3b24d70c556b7e84883e9d99eba71
Alex dos Santos <s2it_alsantos@uolinc.com>
2020-02-06 22:19:07 -0300

    Personalizando a documentação dos endpoints

--- src/main/java/com/alexleko/barberstime/config/SwaggerConfig.java
+++ src/main/java/com/alexleko/barberstime/config/SwaggerConfig.java
@@ -3,28 +3,68 @@ package com.alexleko.barberstime.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Header;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableSwagger2WebMvc
 @Import(SpringDataRestConfiguration.class)
 public class SwaggerConfig {
 
+    // Api Documentation Info
     private static final String RESOURCES = "com.alexleko.barberstime.resources";
     private static final String INFO_TITLE = "Api Documentation Barber's Time";
     private static final String INFO_DESCRIPTION = "Barber's Time store management API";
     private static final String INFO_API_VERSION = "1.0";
 
+    // Global Messages
+    private static final String MSG_UPDATE = "Updated";
+    private static final String MSG_DELETE = "Deleted";
+    private static final String MSG_NOT_AUTHORIZED = "Access Not authorized";
+    private static final String MSG_NOT_FOUND = "Not Found";
+    private static final String MSG_VALIDATION_ERROR = "Validation Error";
+    private static final String MSG_UNEXPECTED_ERROR = "Sorry! Unexpected Error";
+
+    // Header Location Message  - Post
+    private static final String POST_HEADER_LOCATION = "location";
+    private static final String POST_HEADER_NEW_URI = "New resource URI";
+    private static final String POST_HEADER_MESSAGE = "Resource Criated";
+
+    // Global Messages Definition
+    private final ResponseMessage statusCode_201 = customHeaderLocationMessage();
+    private final ResponseMessage statusCode_204_PUT = globalMessage(HttpStatus.NO_CONTENT.value(), MSG_UPDATE);
+    private final ResponseMessage statusCode_204_DEL = globalMessage(HttpStatus.NO_CONTENT.value(), MSG_DELETE);
+    private final ResponseMessage statusCode_403 = globalMessage(HttpStatus.FORBIDDEN.value(), MSG_NOT_AUTHORIZED);
+    private final ResponseMessage statusCode_404 = globalMessage(HttpStatus.NOT_FOUND.value(), MSG_NOT_FOUND);
+    private final ResponseMessage statusCode_422 = globalMessage(HttpStatus.UNPROCESSABLE_ENTITY.value(), MSG_VALIDATION_ERROR);
+    private final ResponseMessage statusCode_500 = globalMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(), MSG_UNEXPECTED_ERROR);
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, Arrays.asList(statusCode_403, statusCode_404, statusCode_500))
+                .globalResponseMessage(RequestMethod.POST, Arrays.asList(statusCode_201, statusCode_403, statusCode_422, statusCode_500))
+                .globalResponseMessage(RequestMethod.PUT, Arrays.asList(statusCode_204_PUT, statusCode_403, statusCode_404, statusCode_422, statusCode_500))
+                .globalResponseMessage(RequestMethod.DELETE, Arrays.asList(statusCode_204_DEL, statusCode_403, statusCode_404, statusCode_500))
+
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(RESOURCES))
@@ -37,6 +77,27 @@ public class SwaggerConfig {
                 .title(INFO_TITLE)
                 .description(INFO_DESCRIPTION)
                 .version(INFO_API_VERSION)
+                .build();
+    }
+
+    private ResponseMessage globalMessage(int code, String msg) {
+        return new ResponseMessageBuilder().code(code).message(msg).build();
+    }
+
+    private ResponseMessage customHeaderLocationMessage() {
+
+        Map<String, Header> map = new HashMap<>();
+        map.put(POST_HEADER_LOCATION,
+                new Header(POST_HEADER_LOCATION,
+                            POST_HEADER_NEW_URI,
+                            new ModelRef("string")
+                )
+        );
+
+        return new ResponseMessageBuilder()
+                .code(HttpStatus.CREATED.value())
+                .message(POST_HEADER_MESSAGE)
+                .headersWithDescription(map)
                 .build();
     }
 
--- src/main/java/com/alexleko/barberstime/resources/CategoryResource.java
+++ src/main/java/com/alexleko/barberstime/resources/CategoryResource.java
@@ -5,6 +5,9 @@ import com.alexleko.barberstime.dto.CategoryDTO;
 import com.alexleko.barberstime.resources.enumerators.ActionTypeFromRequest;
 import com.alexleko.barberstime.resources.hateoas.PresentsHateoasResourcesDTO;
 import com.alexleko.barberstime.services.CategoryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.hateoas.Link;
@@ -30,6 +33,7 @@ public class CategoryResource {
     @Autowired
     private CategoryService categoryService;
 
+    @ApiOperation("Information about category collection features")
     @RequestMapping(method = RequestMethod.OPTIONS)
     public ResponseEntity<PresentsHateoasResourcesDTO> collectionOptions() {
 
@@ -52,6 +56,7 @@ public class CategoryResource {
                 .body(presentation);
     }
 
+    @ApiOperation("Information about category features")
     @RequestMapping(value = "/{id}", method = RequestMethod.OPTIONS)
     public ResponseEntity<PresentsHateoasResourcesDTO> singleOptions(@PathVariable Long id) {
 
@@ -84,6 +89,7 @@ public class CategoryResource {
                 .body(presentation);
     }
 
+    @ApiOperation("Add a new category")
     @PostMapping(produces = { MediaType.APPLICATION_JSON_VALUE },
                     consumes = { MediaType.APPLICATION_JSON_VALUE }  )
     public ResponseEntity<CategoryDTO> insert(@Valid @RequestBody final CategoryDTO paramCategoryDTO) {
@@ -105,6 +111,7 @@ public class CategoryResource {
         return ResponseEntity.created(uri).body(categoryDTO);
     }
 
+    @ApiOperation("Update a category")
     @PutMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE },
                                     consumes = { MediaType.APPLICATION_JSON_VALUE } )
     public ResponseEntity<PresentsHateoasResourcesDTO> update(@Valid @RequestBody CategoryDTO paramCategoryDTO, @PathVariable Long id) {
@@ -122,12 +129,18 @@ public class CategoryResource {
         return ResponseEntity.ok().body(categoryDTO);
     }
 
+    @ApiOperation("Delete a category")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Could not delete a category with works"),
+            @ApiResponse(code = 404, message = "Invalid ID")
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         categoryService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation("Retrieve a category by id")
     @GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE } )
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         Category category = categoryService.findById(id);
@@ -141,6 +154,7 @@ public class CategoryResource {
         return ResponseEntity.ok().body(categoryDTO);
     }
 
+    @ApiOperation(value = "Retrieve all categories", notes = "Note on all categories search feature")
     @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE } )
     public ResponseEntity<List<CategoryDTO>> findAll() {
         List<Category> categories = categoryService.findAll();
@@ -157,6 +171,7 @@ public class CategoryResource {
         return ResponseEntity.ok().body(listDTO);
     }
 
+    @ApiOperation("Retrieve categories with pagination")
     @GetMapping(value = "/page", produces = { MediaType.APPLICATION_JSON_VALUE } )
     public ResponseEntity<Page<CategoryDTO>> findPage(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
