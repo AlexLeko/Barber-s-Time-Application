ee65cc72e4b0e5f0342824cbf6cb4db9da31631b
AlexLeko <lk.alexds@gmail.com>
2020-02-16 18:45:46 -0300

    merge version

---
+++ src/test/java/com/alexleko/barberstime/test/resources/CategoryResourceTest.java
@@ -0,0 +1,28 @@
+package com.alexleko.barberstime.test.resources;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class CategoryResourceTest {
+
+    @Autowired
+    private MockMvc mvc;git
+
+    @Test
+    @DisplayName("Should create a new category")
+    public void createMockTest(){
+
+    }
+
+}
